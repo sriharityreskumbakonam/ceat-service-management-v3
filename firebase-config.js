706669d5c36f223555aea54b738afa42b70de67b
sriharityreskumbakonam <sriharityreskumbakonam@gmail.com>
2026-09-01 21:05:46 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Sri Hari Tyres – Firebase Configuration
+// Project: ceat-service-management
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKWITST9Xl-RyS-K8WMHcgZ9xxyKfBmlo",
+  authDomain: "ceat-service-management.firebaseapp.com",
+  databaseURL: "https://ceat-service-management-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ceat-service-management",
+  storageBucket: "ceat-service-management.firebasestorage.app",
+  messagingSenderId: "498531760991",
+  appId: "1:498531760991:web:11818a77a9185ce316d4ea"
+};
