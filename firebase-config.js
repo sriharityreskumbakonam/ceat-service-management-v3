8537085ffe7f91bd849eed330668f901be450ffb
sriharityreskumbakonam <sriharityreskumbakonam@gmail.com>
2026-09-01 12:22:29 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Sri Hari Tyres – Firebase Configuration
+// Project: queue-6207c
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCu2SYYbzbOVgTeb2s3xAL2VRVHRnT5kfY",
+  authDomain: "queue-6207c.firebaseapp.com",
+  databaseURL: "https://queue-6207c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "queue-6207c",
+  storageBucket: "queue-6207c.firebasestorage.app",
+  messagingSenderId: "792502827865",
+  appId: "1:792502827865:web:52bdc851c3900b1e866a48"
+};
